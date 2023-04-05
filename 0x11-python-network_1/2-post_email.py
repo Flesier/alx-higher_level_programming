678aae425e1fc774bed6f3ec109bf6d5017dc09d
Flesier <flesieronyango18@gmail.com>
2023-04-05 00:22:15 -0700

    POST request dispalay email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-POST request to the passed URL with the email as a parameter
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
 """
-import urllib.request
+from urllib import request, parse
 import sys
 
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
     data = data.encode('ascii')
-    reqq = urllib.request.Request(sys.argv[1], data)
-
-    try:
-        with urllib.request.urlopen(reqq) as resp:
-            if resp is not None:
-                html = resp.read()
-                print(html.decode('utf-8'))
-    except Exception:
-        pass
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
