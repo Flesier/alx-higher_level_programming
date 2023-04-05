8419e452a99b0c988081f7761d46431da36bf146
Flesier <flesieronyango18@gmail.com>
2023-04-05 00:46:19 -0700

    displays body of response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
 Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response.
+sends a POST request to the passed URL with the email,
+and displays the body of the response
 """
-
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    eemail = {'email': sys.argv[2]}
-    resp = requests.post(sys.argv[1], eemail)
-    print("{}".format(resp.text))
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
