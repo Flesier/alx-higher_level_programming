5f4ff8342a577c239c8c980ba22e12ec1791dca0
Flesier <flesieronyango18@gmail.com>
2023-04-04 23:39:48 -0700

    takes url and display methhods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
+# displays all HTTP methods a server will accept
 curl -s -I -X OPTIONS "$1" | grep -i Allow | cut -d ' ' -f2-
