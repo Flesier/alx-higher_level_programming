acd5627387a3cf9be0c4df4a5b1e2670b6acf104
Flesier <flesieronyango18@gmail.com>
2023-04-26 09:43:53 +0000

    script gets contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,3 +1,14 @@
-t fs = require('fs');
+#!/usr/bin/node
+// A script that reads and prints the content of a file
+
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+const fileLocation = process.argv[3];
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  fs.writeFile(fileLocation, body, err => {
+    if (err) console.error(err);
+  });
+});
